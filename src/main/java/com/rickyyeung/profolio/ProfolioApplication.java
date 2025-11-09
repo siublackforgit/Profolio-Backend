3401abf7ce682a6888bac07654f01a2a9484597a
Yeung Ka Chun <siublackbusiness@gmail.com>
2025-11-09 16:14:28 +0800

    initial commit

---
+++ src/main/java/com/rickyyeung/profolio/ProfolioApplication.java
@@ -0,0 +1,13 @@
+package com.rickyyeung.profolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rickyyeung/profolio/ProfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickyyeung.profolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
