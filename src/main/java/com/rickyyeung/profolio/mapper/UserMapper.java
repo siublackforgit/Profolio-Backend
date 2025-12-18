54224209fcf43f5dbf21bbe559d197d63664d7d7
Yeung Ka Chun <siublackbusiness@gmail.com>
2025-12-18 20:46:41 +0800

    Feat : init user repository

--- src/main/java/com/rickyyeung/profolio/mapper/UserMapper.java
+++ src/main/java/com/rickyyeung/profolio/mapper/UserMapper.java
@@ -1,12 +1,25 @@
 package com.rickyyeung.profolio.mapper;
 
 import com.rickyyeung.profolio.model.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
 
-    @Select("SELECT * FROM user WHERE userId = #{id}")
-    User findById(Long id);
+    @Insert("INSERT INTO users (email, password_hash, googleId, display_name, avatarUrl, createdDate, createdBy, lastUpdatedDate, lastUpdatedBy)" +
+            "VALUES (#{email}, #{passwordHash}, #{googleId}, #{displayName}, #{avatarUrl}, #{createdDate}, #{createdBy} , #{lastUpdatedDate}, #{lastUpdatedBy} )")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
+    void insertUser(User user);
+
+    @Select("SELECT * FROM users WHERE email = #{email}")
+    Optional<User> findByEmail(String email);
+
+    @Select("SELECT * FROM users WHERE google_id = #{googleId}")
+    Optional<User> findByGoogleId(String googleId);
+
+    @Update("UPDATE users SET displayName = #{displayName}, avatarUrl = #{avatarUrl},  lastUpdatedBy = #{lastUpdatedBy}, lastUpdatedDate = Now() " +
+            "WHERE userId = #{userId}")
+    void updateUser(User user);
 }
