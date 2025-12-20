b7f2cca3bcfa8a402448fd087902ad5bcfe2501f
Yeung Ka Chun <siublackbusiness@gmail.com>
2025-12-20 14:28:25 +0800

    feat : init email Service

--- src/main/java/com/rickyyeung/profolio/service/EmailService.java
+++ src/main/java/com/rickyyeung/profolio/service/EmailService.java
@@ -1,2 +1,27 @@
-package com.rickyyeung.profolio.service;public class EmailService {
+package com.rickyyeung.profolio.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private  JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(String to, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText(body);
+        mailSender.send(msg);
+    }
 }
