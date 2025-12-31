eccdc74edd1e6b3f756aa915dd40e2c856278f09
Yeung Ka Chun <siublackbusiness@gmail.com>
2025-12-31 21:14:39 +0800

    feat : init the userMapper

--- src/main/java/com/rickyyeung/profolio/mapper/UserMapper.java
+++ src/main/java/com/rickyyeung/profolio/mapper/UserMapper.java
@@ -8,18 +8,18 @@ import java.util.Optional;
 @Mapper
 public interface UserMapper {
 
-    @Insert("INSERT INTO users (email, passwordHash, googleId, displayName, avatarUrl, createdDate, createdBy, lastUpdatedDate, lastUpdatedBy)" +
+    @Insert("INSERT INTO user (email, passwordHash, googleId, displayName, avatarUrl, createdDate, createdBy, lastUpdatedDate, lastUpdatedBy)" +
             "VALUES (#{email}, #{passwordHash}, #{googleId}, #{displayName}, #{avatarUrl}, NOW(), #{createdBy} , NOW() , #{lastUpdatedBy} )")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
     void insertUser(User user);
 
-    @Select("SELECT * FROM users WHERE email = #{email}")
+    @Select("SELECT * FROM user WHERE email = #{email}")
     Optional<User> findByEmail(String email);
 
-    @Select("SELECT * FROM users WHERE google_id = #{googleId}")
+    @Select("SELECT * FROM user WHERE google_id = #{googleId}")
     Optional<User> findByGoogleId(String googleId);
 
-    @Update("UPDATE users SET " +
+    @Update("UPDATE user SET " +
             "email = #{email}, " +
             "passwordHash = #{passwordHash}, " +
             "googleId = #{googleId}, " +
